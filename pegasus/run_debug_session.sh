4f671b60b2e7d75c5123a8bdb8e45b1288639494
chenkaixusan <chenkaixusan@gmail.com>
2025-06-25 14:18:40 +0000

    移除调试脚本中的 SCRIPT_PATH 变量

--- pegasus/run_debug_session.sh
+++ pegasus/run_debug_session.sh
@@ -3,7 +3,6 @@
 # === 1. 参数设定 ===
 PROJECT_NAME="SKIING"                 # 你的项目名（必须改）
 ENV_PATH="/home/SKIING/chenkaixu/.venv/med_atn"  # venv 虚拟环境路径（或用 conda 环境名）
-SCRIPT_PATH="train.py"                 # 你要测试的 Python 文件
 TIME="01:00:00"                        # 申请时长（最大 01:00:00）
 
 # === 2. 启动 debug 节点会话 ===
