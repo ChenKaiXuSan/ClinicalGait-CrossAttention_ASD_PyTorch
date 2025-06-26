c3331effb2d2adfea94f912ae45663460784b914
chenkaixu <chenkaixusan@gmail.com>
2025-06-26 12:48:11 +0900

    更新 run_train_se_atn.sh，注释掉预训练模型下载命令；修改 train_3dcnn.py，注释掉学习率调度器的 verbose 参数

--- pegasus/run_train_se_atn.sh
+++ pegasus/run_train_se_atn.sh
@@ -13,7 +13,7 @@ mkdir -p logs/pegasus/
 mkdir -p checkpoints/
 
 # === 下载预训练模型（如果需要） ===
-wget -O /home/SKIING/chenkaixu/code/ClinicalGait-CrossAttention_ASD_PyTorch/checkpoints/SLOW_8x8_R50.pyth https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/kinetics/SLOW_8x8_R50.pyth
+# wget -O /home/SKIING/chenkaixu/code/ClinicalGait-CrossAttention_ASD_PyTorch/checkpoints/SLOW_8x8_R50.pyth https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/kinetics/SLOW_8x8_R50.pyth
 
 # === 加载 Python + 激活 Conda 环境 ===
 module load intelpython/2022.3.1
